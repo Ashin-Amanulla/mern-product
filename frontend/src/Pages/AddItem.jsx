d14ac7bfb4d0703ca9ff7e009cec52803c7be59d
Ashin <ashin.jnv@gmail.com>
2023-05-26 14:34:08 +0530

    use navigate

--- frontend/src/Pages/AddItem.jsx
+++ frontend/src/Pages/AddItem.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+
+
 
 export default function AddItem() {
   return (
-    <div>AddItem</div>
+    <div>
+ <Container className="d-flex justify-content-center">
+        <h1 className="mx-3"> Add Products</h1>
+    
+      </Container>
+
+
+
+    </div>
   )
 }
